01c5e8ee064197f7c79ef6f449132077dc2f9e01
Lance Finfrock <lfinfrock@chef.io>
2020-06-13 13:57:29 -0700

    Allowing http and https
    Signed-off-by: Lance Finfrock <lfinfrock@chef.io>

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -23,8 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = "https://".$_SERVER['HTTP_HOST'];
-
+$config['base_url'] = (isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
 /*
 |--------------------------------------------------------------------------
 | Index File
